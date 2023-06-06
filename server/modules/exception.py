26e91db2971609efa84fc32e2008d8b5c01d202b
maumyvalkin <arklightx@yandex.ru>
2023-06-06 20:58:29 +0400

    Убраны лишние блоки кода

---
+++ server/modules/exception.py
@@ -0,0 +1,8 @@
+class ClientException(Exception):
+    resp_status = 400
+    message = ""
+
+    def __init__(self, message="Произошла непредвиденная ошибка", resp_status=400):
+        self.resp_status = resp_status
+        self.message = message
+        super().__init__(message)
--- server/modules/user/auth.py
+++ server/modules/user/auth.py
@@ -1,11 +1,94 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.request import Request
+from datetime import datetime
+import jwt
+from django.conf import settings
+from modules.exception import ClientException
 
-from core.exception import ClientException
-from core.security import jwt
-from core.security.jwt import AccessToken
+from .models import User, AccessTokens, RefreshTokens
 
-from .models import User, AccessTokens
+class BaseToken:
+    token: str | None
+    payload: dict
+    is_valid: bool
+    exp_delta: datetime.time
+
+    def __init__(self, token: str, payload: dict):
+        self.token = token
+        self.payload = payload
+        self.is_valid = False
+
+
+class AccessToken(BaseToken):
+    exp_delta = settings.JWT_ACCESS_LIFETIME
+
+
+class RefreshToken(BaseToken):
+    exp_delta = settings.JWT_REFRESH_LIFETIME
+
+
+class JWTPair:
+    __access: AccessToken | None
+    __refresh: RefreshToken | None
+
+    def __init__(self, access: AccessToken = None, refresh: RefreshToken = None):
+        self.__access = access
+        self.__refresh = refresh
+
+    @property
+    def access(self) -> AccessToken:
+        return self.__access
+
+    @access.setter
+    def access(self, access: AccessToken):
+        self.__access = access
+
+    @property
+    def refresh(self) -> RefreshToken:
+        return self.__refresh
+
+    @refresh.setter
+    def refresh(self, refresh: RefreshToken):
+        self.__refresh = refresh
+
+    def to_dict(self):
+        return {"access": self.__access, "refresh": self.__refresh}
+
+
+def encode_jwt(payload: dict, t_type: str) -> AccessToken | RefreshToken:
+    if t_type == "a":
+        payload["exp_time"] = str(datetime.now() + settings.JWT_ACCESS_LIFETIME)
+        payload["t_type"] = "a"
+    else:
+        payload["exp_time"] = str(datetime.now() + settings.JWT_REFRESH_LIFETIME)
+        payload["t_type"] = "r"
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+    encoded_token = AccessToken(token, payload) if payload["t_type"] == "a" else RefreshToken(token, payload)
+    encoded_token.is_valid = True
+    return encoded_token
+
+
+def decode_jwt(token: str) -> AccessToken | RefreshToken:
+    info = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"], options={"verify_signature": True})
+    decoded_token = AccessToken(token, info) if info.get("t_type") == "a" else RefreshToken(token, info)
+    decoded_token.is_valid = datetime.strptime(info.get("exp_time"), "%Y-%m-%d %H:%M:%S.%f") > datetime.now()
+    return decoded_token
+
+
+def generate_jwt_pair(user: User) -> JWTPair:
+    access_info = {"id": user.id}
+    refresh_info = {"id": user.id}
+
+    access_token = encode_jwt(access_info, "a")
+    refresh_token = encode_jwt(refresh_info, "r")
+    acc_token_model = AccessTokens.objects.create(token=access_token.token,
+                                                  user_id=user.id,
+                                                  valid_to=(datetime.now() + access_token.exp_delta))
+    RefreshTokens.objects.create(token=refresh_token.token,
+                                 user_id=user.id,
+                                 valid_to=(datetime.now() + refresh_token.exp_delta),
+                                 access_id=acc_token_model.token)
+    return JWTPair(access_token, refresh_token)
 
 
 class AuthenticationSystem(BaseAuthentication):
@@ -15,7 +98,7 @@ class AuthenticationSystem(BaseAuthentication):
             if header is None:
                 return None
             header_token = header.split(" ")[1]
-            encoded_token = jwt.decode_jwt(header_token)
+            encoded_token = decode_jwt(header_token)
             if not encoded_token.is_valid:
                 return None
         except Exception as e:
